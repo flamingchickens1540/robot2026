ac3976ac8ff5598921f25d71975598aeca06a07c
Nebula0128 <perdues@catlin.edu>
2026-01-27 16:35:19 -0800

    Finished IntakeIO

--- src/main/java/org/team1540/robot2026/subsystems/intake/IntakeIO.java
+++ src/main/java/org/team1540/robot2026/subsystems/intake/IntakeIO.java
@@ -10,9 +10,21 @@ public interface IntakeIO {
         public double spinSupplyCurrentAmps = 0;
         public double spinStatorCurrentAmps = 0;
         public boolean spinConnected = true;
+
+        public boolean pivotConnected = true;
+        public Rotation2d pivotPosition = Rotation2d.kZero;
+        public double pivotMotorVelocityRPS = 0;
+        public double pivotMotorAppliedVolts = 0;
+        public double pivotSupplyCurrentAmps = 0;
+        public double pivotStatorCurrentAmps = 0;
+
     }
 
     default void setIntakeVoltage(double voltage) {}
 
     default void updateInputs(IntakeInputs inputs) {}
+
+    default void setPitvotPosition(Rotation2d pitvotPosition) {}
+
+    default void setPivotAppliedVoltage(double voltage) {}
 }
