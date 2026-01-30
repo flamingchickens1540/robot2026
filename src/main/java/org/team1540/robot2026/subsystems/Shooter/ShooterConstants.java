c661e3a3148325ef8e148fc0966ebb3ad4c3091a
chloezhang <zhangch@catlin.edu>
2026-01-29 17:16:22 -0800

    feat: added flywheels

---
+++ src/main/java/org/team1540/robot2026/subsystems/Shooter/ShooterConstants.java
@@ -0,0 +1,14 @@
+package org.team1540.robot2026.subsystems.Shooter;
+
+public class ShooterConstants {
+    public static class Flywheels {
+        public static final int ID = 67;
+
+
+        public static final double KP= 67;
+        public static final double KI= 67;
+        public static final double KD= 67;
+        public static final double KS= 67;
+        public static final double KV= 67;
+    }
+}
