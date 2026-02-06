8462e75499c774303eb3027db225a4fd651a8c3f
Nebula0128 <perdues@catlin.edu>
2026-02-05 17:39:00 -0800

    Almost Finished the IntakeIOTalonFX, pretty much completed updateInputs, and added status signals.

--- src/main/java/org/team1540/robot2026/subsystems/intake/IntakeIOTalonFX.java
+++ src/main/java/org/team1540/robot2026/subsystems/intake/IntakeIOTalonFX.java
@@ -2,25 +2,55 @@ package org.team1540.robot2026.subsystems.intake;
 
 import static org.team1540.robot2026.subsystems.intake.IntakeConstants.*;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 
 public class IntakeIOTalonFX implements IntakeIO {
 
     private TalonFX pivotMotor = new TalonFX(PIVOT_MOTOR_ID);
     private TalonFX intakeMotor = new TalonFX(INTAKE_MOTOR_ID);
 
+    private StatusSignal<AngularVelocity> spinMotorVelocityRPS;
+    private StatusSignal<Voltage> spinMotorAppliedVolts;
+    private StatusSignal<Current> spinSupplyCurrentAmps;
+    private StatusSignal<Current> spinStatorCurrentAmps;
+
+    private StatusSignal<Rotation2d> pivotPosition;
+    private StatusSignal<AngularVelocity> pivotMotorVelocityRPS;
+    private StatusSignal<Voltage> pivotMotorAppliedVolts;
+    private StatusSignal<Current> pivotSupplyCurrentAmps;
+    private StatusSignal<Current> pivotStatorCurrentAmps;
+
     public IntakeIOTalonFX() {
+
+        spinMotorVelocityRPS = intakeMotor.getVelocity();
+        spinMotorAppliedVolts = intakeMotor.getMotorVoltage();
+        spinSupplyCurrentAmps = intakeMotor.getSupplyCurrent();
+        spinStatorCurrentAmps = intakeMotor.getStatorCurrent();
+
+        // pivotPosition = pivotMotor.getPosition(); Why is it mad at me? //TODO: fix this
+        pivotMotorVelocityRPS = pivotMotor.getVelocity();
+        pivotMotorAppliedVolts = pivotMotor.getMotorVoltage();
+        pivotSupplyCurrentAmps = pivotMotor.getSupplyCurrent();
+        pivotStatorCurrentAmps = pivotMotor.getStatorCurrent();
 
         TalonFXConfiguration pivotTalonFXConfig = new TalonFXConfiguration();
         pivotTalonFXConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
         pivotTalonFXConfig.CurrentLimits.withStatorCurrentLimit(120);
         pivotTalonFXConfig.CurrentLimits.withSupplyCurrentLimit(50);
-        pivotTalonFXConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: is it inverted
+        pivotTalonFXConfig.MotorOutput.Inverted =
+                InvertedValue.CounterClockwise_Positive; // TODO: is it inverted or not?
         pivotTalonFXConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
         pivotMotor.getConfigurator().apply(pivotTalonFXConfig);
@@ -29,7 +59,8 @@ public class IntakeIOTalonFX implements IntakeIO {
         intakeTalonFXConfig.CurrentLimits.withStatorCurrentLimitEnable(true);
         intakeTalonFXConfig.CurrentLimits.withStatorCurrentLimit(120);
         intakeTalonFXConfig.CurrentLimits.withSupplyCurrentLimit(50);
-        intakeTalonFXConfig.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: is it inverted
+        intakeTalonFXConfig.MotorOutput.Inverted =
+                InvertedValue.CounterClockwise_Positive; // TODO: is it inverted or not?
         intakeTalonFXConfig.MotorOutput.NeutralMode = NeutralModeValue.Coast;
 
         intakeMotor.getConfigurator().apply(pivotTalonFXConfig);
@@ -48,5 +79,42 @@ public class IntakeIOTalonFX implements IntakeIO {
     }
 
     @Override
-    public void updateInputs(IntakeInputs inputs) {}
+    public void updateInputs(IntakeInputs inputs) {
+        StatusCode spinStatus = BaseStatusSignal.refreshAll(
+                spinMotorVelocityRPS, spinMotorAppliedVolts, spinSupplyCurrentAmps, spinStatorCurrentAmps);
+        StatusCode pivotStatus = BaseStatusSignal.refreshAll(
+                pivotMotorVelocityRPS,
+                pivotPosition,
+                pivotMotorAppliedVolts,
+                pivotSupplyCurrentAmps,
+                pivotStatorCurrentAmps);
+
+        // inputs.spinConnected = spinConnectedDebounce.calculate(spinStatus.isOK()); remake this to work for this
+
+        inputs.spinMotorVelocityRPS = spinMotorVelocityRPS.getValueAsDouble();
+        inputs.spinMotorAppliedVolts = spinMotorAppliedVolts.getValueAsDouble();
+        inputs.spinSupplyCurrentAmps = spinSupplyCurrentAmps.getValueAsDouble();
+        inputs.spinStatorCurrentAmps = spinStatorCurrentAmps.getValueAsDouble();
+
+        // inputs.pivotConnected = pivotConnectedDebounce.calculate(pivotStatus.isOK()); remake that to work for this
+
+        inputs.pivotPosition = pivotPosition.getValue();
+        inputs.pivotMotorVelocityRPS = pivotMotorVelocityRPS.getValueAsDouble();
+        inputs.pivotMotorAppliedVolts = pivotMotorAppliedVolts.getValueAsDouble();
+        inputs.pivotSupplyCurrentAmps = pivotSupplyCurrentAmps.getValueAsDouble();
+        inputs.pivotStatorCurrentAmps = pivotStatorCurrentAmps.getValueAsDouble();
+    }
+
+    public void setIntakeVoltage(double voltage) {
+        // intakeMotor.setControl(intakeMotor(voltage).withOutput(voltage));   this is what they did, I have no idea
+        // what to do next
+    }
+
+    public void setPivotPosition(Rotation2d pivotPosition) {
+        // no idea what to do here
+    }
+
+    public void setPivotAppliedVoltage(double voltage) {
+        // also no idea what to do here
+    }
 }
