cf067f5766eb314bf54339ffb1905cbfd3cd7614
Jack <tylerj@catlin.edu>
2026-02-20 15:45:08 -0800

    Added needed classes

--- src/main/java/org/team1540/robot2026/RobotState.java
+++ src/main/java/org/team1540/robot2026/RobotState.java
@@ -1,12 +1,16 @@
 package org.team1540.robot2026;
 
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.math.interpolation.TimeInterpolatableBuffer;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,8 +18,18 @@ import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.AutoLogOutputManager;
 import org.littletonrobotics.junction.Logger;
 import org.team1540.robot2026.subsystems.drive.DrivetrainConstants;
+import org.team1540.robot2026.subsystems.vision.AprilVisionIO;
+
+import java.util.Optional;
+
+import static org.team1540.robot2026.subsystems.vision.AprilTagVisionConstants.*;
 
 public class RobotState {
+
+    private final Timer resetTimer = new Timer();
+    private final TimeInterpolatableBuffer<Pose2d> fusedPoseBuffer = TimeInterpolatableBuffer.createBuffer(2.0);
+    private final SingleTagPoseEstimate[] singleTagPoses = new SingleTagPoseEstimate[FieldConstants.aprilTagCount];
+
     private static RobotState instance = null;
 
     public static RobotState getInstance() {
@@ -113,4 +127,88 @@ public class RobotState {
     public void addTurretObservation(Rotation2d turretAngle, double timestamp) {
         turretAngleBuffer.addSample(timestamp, turretAngle);
     }
+
+    public boolean addVisionMeasurement(AprilVisionIO.PoseObservation visionPose) {
+        if (shouldAcceptVision(visionPose) && resetTimer.hasElapsed(0.1)) {
+            poseEstimator.addVisionMeasurement(
+                    visionPose.estimatedPoseMeters().toPose2d(), visionPose.timestampSecs(), getStdDevs(visionPose));
+            fusedPoseBuffer.addSample(visionPose.timestampSecs(), poseEstimator.getEstimatedPosition());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean shouldAcceptVision(AprilVisionIO.PoseObservation poseObservation) {
+        Pose3d estimatedPose = poseObservation.estimatedPoseMeters();
+        return poseObservation.numTagsSeen() >= MIN_ACCEPTED_NUM_TAGS // Must see sufficient tags
+                && (poseObservation.numTagsSeen() > 1
+                || poseObservation.ambiguity() < MAX_AMBIGUITY) // Must be multiple tags or low ambiguity
+                // Must be within field roughly
+                && estimatedPose.getX() >= -MAX_OUTSIDE_OF_FIELD_TOLERANCE
+                && estimatedPose.getX() <= FieldConstants.fieldLength + MAX_OUTSIDE_OF_FIELD_TOLERANCE
+                && estimatedPose.getY() >= -MAX_OUTSIDE_OF_FIELD_TOLERANCE
+                && estimatedPose.getY() <= FieldConstants.fieldWidth + MAX_OUTSIDE_OF_FIELD_TOLERANCE
+                // Must not be actively flying
+                && Math.abs(estimatedPose.getZ()) <= MAX_ROBOT_Z_TOLERANCE;
+    }
+
+    private Matrix<N3, N1> getStdDevs(AprilVisionIO.PoseObservation poseObservation) {
+        double xyStdDev =
+                XY_STD_DEV_COEFF * Math.pow(poseObservation.avgTagDistance(), 2.0) / poseObservation.numTagsSeen();
+        double rotStdDev =
+                ROT_STD_DEV_COEFF * Math.pow(poseObservation.avgTagDistance(), 2.0) / poseObservation.numTagsSeen();
+        return VecBuilder.fill(
+                xyStdDev,
+                xyStdDev,
+                DriverStation.isEnabled() && poseObservation.numTagsSeen() <= 1 ? Double.POSITIVE_INFINITY : rotStdDev);
+    }
+
+    public void addSingleTagMeasurement(AprilVisionIO.SingleTagObservation observation) {
+        int tagIndex = observation.id() - 1;
+
+        // Skip if measurement is not the most recent
+        if (singleTagPoses[tagIndex] != null && singleTagPoses[tagIndex].timestamp() >= observation.timestampSecs()) {
+            return;
+        }
+
+        Optional<Pose2d> poseBufferSample = fusedPoseBuffer.getSample(observation.timestampSecs());
+        if (poseBufferSample.isEmpty()) {
+            return;
+        }
+        Rotation2d robotRotation = poseBufferSample.get().getRotation();
+
+        Pose3d robotToCameraPose = Pose3d.kZero.transformBy(observation.cameraTransform());
+
+        Translation2d cameraToTagTranslation = new Pose3d(
+                Translation3d.kZero,
+                new Rotation3d(
+                        0.0,
+                        -observation.pitch().getRadians(),
+                        -observation.yaw().getRadians()))
+                .transformBy(new Transform3d(observation.distanceMeters(), 0, 0, Rotation3d.kZero))
+                .getTranslation()
+                .rotateBy(new Rotation3d(
+                        0, observation.cameraTransform().getRotation().getY(), 0))
+                .toTranslation2d();
+        Rotation2d cameraToTagRotation =
+                robotRotation.plus(robotToCameraPose.toPose2d().getRotation().plus(cameraToTagTranslation.getAngle()));
+        Optional<Pose3d> tagPose = FieldConstants.aprilTagLayout.getTagPose(observation.id());
+        if (tagPose.isEmpty()) return;
+        Translation2d fieldToCameraTranslation = new Pose2d(
+                tagPose.get().toPose2d().getTranslation(), cameraToTagRotation.plus(Rotation2d.k180deg))
+                .transformBy(new Transform2d(cameraToTagTranslation.getNorm(), 0.0, Rotation2d.kZero))
+                .getTranslation();
+        Pose2d robotPose = new Pose2d(
+                fieldToCameraTranslation,
+                robotRotation.plus(robotToCameraPose.toPose2d().getRotation()))
+                .transformBy(new Transform2d(robotToCameraPose.toPose2d(), Pose2d.kZero));
+        robotPose = new Pose2d(robotPose.getTranslation(), robotRotation);
+
+        singleTagPoses[tagIndex] =
+                new SingleTagPoseEstimate(robotPose, observation.distanceMeters(), observation.timestampSecs());
+    }
+
+    public record SingleTagPoseEstimate(Pose2d pose, double distance, double timestamp) {}
+
+
 }
---
+++ src/main/java/org/team1540/robot2026/subsystems/vision/AprilTagVision.java
@@ -0,0 +1,95 @@
+package org.team1540.robot2026.subsystems.vision;
+
+import static org.team1540.robot2026.subsystems.vision.AprilTagVisionConstants.*;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.littletonrobotics.junction.Logger;
+import org.team1540.robot2026.Constants;
+// import org.team1540.robot2026.RobotState;
+import org.team1540.robot2026.FieldConstants;
+import org.team1540.robot2026.RobotState;
+import org.team1540.robot2026.subsystems.vision.AprilVisionIO.PoseObservation;
+import org.team1540.robot2026.util.LoggedTracer;
+
+public class AprilTagVision extends SubsystemBase {
+    private final AprilVisionIO[] visionIOs;
+    private final AprilTagVisionIOInputsAutoLogged[] cameraInputs;
+
+    private final Alert[] disconnectedAlerts;
+
+    private final ArrayList<Pose3d> lastAcceptedPoses = new ArrayList<>();
+    private final ArrayList<Pose3d> lastRejectedPoses = new ArrayList<>();
+    private final ArrayList<Pose3d> lastSeenTagPoses = new ArrayList<>();
+
+    private AprilTagVision(AprilVisionIO... visionIOs) {
+        this.visionIOs = visionIOs;
+
+        this.cameraInputs = new AprilTagVisionIOInputsAutoLogged[visionIOs.length];
+        this. disconnectedAlerts = new Alert[visionIOs.length];
+        for (int i = 0; i < cameraInputs.length; i++) {
+            cameraInputs[i] = new AprilTagVisionIOInputsAutoLogged();
+            disconnectedAlerts[i] = new Alert(visionIOs[i].name + " is disconnected.", Alert.AlertType.kWarning);
+        }
+    }
+
+    public void periodic() {
+        LoggedTracer.reset();
+
+        for (int i = 0; i < visionIOs.length; i++) {
+            visionIOs[i].updateInputs(cameraInputs[i]);
+            Logger.processInputs("Vision/" + visionIOs[i].name, cameraInputs[i]);
+        }
+
+        RobotState robotState = RobotState.getInstance();
+
+        lastAcceptedPoses.clear();
+        lastRejectedPoses.clear();
+        lastSeenTagPoses.clear();
+        for (int i = 0; i < visionIOs.length; i++) {
+            disconnectedAlerts[i].set(!cameraInputs[i].connected);
+
+            for (PoseObservation observation : cameraInputs[i].poseObservations) {
+                if (robotState.addVisionMeasurement(observation)) {
+                    lastAcceptedPoses.add(observation.estimatedPoseMeters());
+                } else {
+                    lastRejectedPoses.add(observation.estimatedPoseMeters());
+                }
+
+                for (AprilVisionIO.SingleTagObservation observationx : cameraInputs[i].singleTagObservations) {
+                    robotState.addSingleTagMeasurement(observationx);
+                }
+
+                lastSeenTagPoses.addAll(Arrays.stream(cameraInputs[i].seenTagIDs)
+                        .mapToObj(tagID ->
+                                FieldConstants.AprilTagLayoutType.OFFICIAL.getLayout().getTagPose(tagID).orElse(Pose3d.kZero))
+                        .toList());
+            }
+        }
+
+        Logger.recordOutput("Vision/AcceptedPoses", lastAcceptedPoses.toArray(new Pose3d[0]));
+        Logger.recordOutput("Vision/RejectedPoses", lastRejectedPoses.toArray(new Pose3d[0]));
+        Logger.recordOutput("Vision/SeenTagPoses", lastSeenTagPoses.toArray(new Pose3d[0]));
+
+        LoggedTracer.record("AprilTagVision");
+    }
+    public static AprilTagVision createReal() {
+        return new AprilTagVision(
+                new AprilTagVisionIOPhoton(FL_CAMERA_NAME, FL_CAMERA_TRANSFORM),
+                new AprilTagVisionIOPhoton(FR_CAMERA_NAME, FR_CAMERA_TRANSFORM),
+                new AprilTagVisionIOPhoton(BL_CAMERA_NAME, BL_CAMERA_TRANSFORM),
+                new AprilTagVisionIOPhoton(BR_CAMERA_NAME, BR_CAMERA_TRANSFORM));
+    }
+
+
+    public static AprilTagVision createDummy() {
+        return new AprilTagVision(
+                new AprilVisionIO(FL_CAMERA_NAME) {},
+                new AprilVisionIO(FR_CAMERA_NAME) {},
+                new AprilVisionIO(BL_CAMERA_NAME) {},
+                new AprilVisionIO(BR_CAMERA_NAME) {});
+    }
+}
