59ef72d78b51eaeba955178849e6a91a81b21a43
Luna Gonzalez Gonzalez <gonzalezgonzalezl@catlin.edu>
2026-02-20 21:46:51 -0800

    feat: LEDs

--- src/main/java/org/team1540/robot2026/subsystems/leds/LEDConstants.java
+++ src/main/java/org/team1540/robot2026/subsystems/leds/LEDConstants.java
@@ -1,6 +1,6 @@
 package org.team1540.robot2026.subsystems.leds;
 
 public class LEDConstants {
-    public static final int LED_PORT = 67;
-    public static final int LED_LENGTH = 67;
+    public static final int LEDS_PWM_PORT = 9;
+    public static final int LEDS_LENGTH = 55;
 }
--- src/main/java/org/team1540/robot2026/subsystems/leds/LEDs.java
+++ src/main/java/org/team1540/robot2026/subsystems/leds/LEDs.java
@@ -1,45 +1,36 @@
 package org.team1540.robot2026.subsystems.leds;
 
-import static org.team1540.robot2026.subsystems.leds.LEDConstants.LED_LENGTH;
-import static org.team1540.robot2026.subsystems.leds.LEDConstants.LED_PORT;
+import static edu.wpi.first.units.Units.Second;
+import static org.team1540.robot2026.subsystems.leds.LEDConstants.LEDS_LENGTH;
+import static org.team1540.robot2026.subsystems.leds.LEDConstants.LEDS_PWM_PORT;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import static edu.wpi.first.units.Units.Value;
+
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
+
 import org.team1540.robot2026.util.LoggedTracer;
 
 public class LEDs extends SubsystemBase {
-    private final AddressableLED ledStrip = new AddressableLED(LED_PORT);
-    private final AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(LED_LENGTH);
+    private final AddressableLED ledStrip = new AddressableLED(LEDS_PWM_PORT);
+    private final AddressableLEDBuffer buffer = new AddressableLEDBuffer(LEDS_LENGTH);
 
-    private final List<LedWindow> children = new ArrayList<LedWindow>();
+    private final List<LedWindow> children = new ArrayList<>();
+    public final LedWindow viewFull = new LedWindow(0, LEDS_LENGTH - 1);
+    public final LedWindow viewTop = new LedWindow(LEDS_LENGTH - 6, LEDS_LENGTH - 1);
 
     public LEDs() {
-        ledStrip.setLength(ledBuffer.getLength());
+        ledStrip.setLength(buffer.getLength());
         ledStrip.start();
-    }
-
-    public class LedWindow extends SubsystemBase {
-        private final AddressableLEDBufferView view;
-        private LEDPattern defaultpattern;
-        private LEDPattern activepattern;
-
-        private LedWindow(int start, int end) {
-            view = ledBuffer.createView(start, end);
-            children.add(this);
-            this.setName("LedWindow " + start + " to " + end);
-        }
-
-        private void apply() {
-            if (activepattern != null) {
-                this.activepattern.applyTo(view);
-            } else if (defaultpattern != null) {
-                this.activepattern.applyTo(view);
-            }
-        }
+        viewFull.setDefaultPattern(CustomLEDPatterns.movingRainbow(Value.one().div(Second.of(5))));
     }
 
     @Override
@@ -49,9 +40,57 @@ public class LEDs extends SubsystemBase {
         for (LedWindow child : children) {
             child.apply();
         }
-
-        ledStrip.setData(ledBuffer);
+        ledStrip.setData(buffer);
 
         LoggedTracer.record("LEDs");
+    }
+
+    public static Color getAllianceColor() {
+        return DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Blue
+                ? Color.kBlue
+                : Color.kRed;
+    }
+
+    public class LedWindow extends SubsystemBase {
+        private final AddressableLEDBufferView view;
+        private LEDPattern activePattern;
+        private LEDPattern defaultPattern;
+
+        private LedWindow(int start, int end) {
+            view = buffer.createView(start, end);
+            children.add(this);
+            this.setName("LedWindow" + start + "to" + end);
+        }
+
+        private void apply() {
+            if (activePattern != null) {
+                this.activePattern.applyTo(view);
+            } else if (defaultPattern != null) {
+                this.defaultPattern.applyTo(view);
+            }
+        }
+
+        public Command commandShowPattern(LEDPattern pattern) {
+            return Commands.startEnd(() -> this.activePattern = pattern, () -> this.activePattern = null, this)
+                    .ignoringDisable(true);
+        }
+
+        public Command commandShowPattern(Supplier<LEDPattern> pattern) {
+            return Commands.startEnd(() -> this.activePattern = pattern.get(), () -> this.activePattern = null, this)
+                    .ignoringDisable(true);
+        }
+
+        public void setDefaultPattern(LEDPattern pattern) {
+            this.defaultPattern = pattern;
+        }
+
+        public Command commandDefaultPattern(Supplier<LEDPattern> pattern) {
+            return Commands.runOnce(() -> this.setDefaultPattern(pattern.get())).ignoringDisable(true);
+        }
+
+        public Command showRSLState() {
+            LEDPattern pattern = LEDPattern.solid(new Color("#ff3700"));
+            return commandShowPattern(pattern.synchronizedBlink(RobotController::getRSLState));
+        }
     }
 }
