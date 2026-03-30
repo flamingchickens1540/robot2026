317de7dac880df75aa0742272fdd6554700017cd
chloezhang <zhangch@catlin.edu>
2026-03-30 16:41:37 -0700

    feat: new spindexer feeder motor!

--- src/main/java/org/team1540/robot2026/subsystems/spindexer/SpindexerIOTalonFX.java
+++ src/main/java/org/team1540/robot2026/subsystems/spindexer/SpindexerIOTalonFX.java
@@ -15,20 +15,28 @@ import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
 
 public class SpindexerIOTalonFX implements SpindexerIO {
-    private final TalonFX spinMotor = new TalonFX(SPIN_MOTOR_ID);
-    private final TalonFX feederMotor = new TalonFX(FEEDER_MOTOR_ID);
+    private final TalonFX spinMotor1 = new TalonFX(SPIN_MOTOR_ID);
+    private final TalonFX feederMotor2 = new TalonFX(FEEDER_MOTOR_ID_2);
+    private final TalonFX feederMotor1 = new TalonFX(FEEDER_MOTOR_ID_1);
 
-    private final StatusSignal<AngularVelocity> spinVelocity = spinMotor.getVelocity();
-    private final StatusSignal<Voltage> spinVoltage = spinMotor.getMotorVoltage();
-    private final StatusSignal<Current> spinSupplyCurrent = spinMotor.getSupplyCurrent();
-    private final StatusSignal<Current> spinStatorCurrent = spinMotor.getStatorCurrent();
-    private final StatusSignal<Temperature> spinTemp = spinMotor.getDeviceTemp();
+    private final StatusSignal<AngularVelocity> spinVelocity = spinMotor1.getVelocity();
+    private final StatusSignal<Voltage> spinVoltage = spinMotor1.getMotorVoltage();
+    private final StatusSignal<Current> spinSupplyCurrent = spinMotor1.getSupplyCurrent();
+    private final StatusSignal<Current> spinStatorCurrent = spinMotor1.getStatorCurrent();
+    private final StatusSignal<Temperature> spinTemp = spinMotor1.getDeviceTemp();
 
-    private final StatusSignal<AngularVelocity> feederVelocity = feederMotor.getVelocity();
-    private final StatusSignal<Voltage> feederVoltage = feederMotor.getMotorVoltage();
-    private final StatusSignal<Current> feederSupplyCurrent = feederMotor.getSupplyCurrent();
-    private final StatusSignal<Current> feederStatorCurrent = feederMotor.getStatorCurrent();
-    private final StatusSignal<Temperature> feederTemp = feederMotor.getDeviceTemp();
+    private final StatusSignal<AngularVelocity> feeder1Velocity = feederMotor1.getVelocity();
+    private final StatusSignal<Voltage> feeder1Voltage = feederMotor1.getMotorVoltage();
+    private final StatusSignal<Current> feeder1SupplyCurrent = feederMotor1.getSupplyCurrent();
+    private final StatusSignal<Current> feeder1StatorCurrent = feederMotor1.getStatorCurrent();
+    private final StatusSignal<Temperature> feeder1Temp = feederMotor1.getDeviceTemp();
+
+    private final StatusSignal<AngularVelocity> feeder2Velocity = feederMotor2.getVelocity();
+    private final StatusSignal<Voltage> feeder2Voltage = feederMotor2.getMotorVoltage();
+    private final StatusSignal<Current> feeder2SupplyCurrent = feederMotor2.getSupplyCurrent();
+    private final StatusSignal<Current> feeder2StatorCurrent = feederMotor2.getStatorCurrent();
+    private final StatusSignal<Temperature> feeder2Temp = feederMotor2.getDeviceTemp();
+
 
     private final VoltageOut spinVoltageCtrlReq = new VoltageOut(0).withEnableFOC(true);
     private final VoltageOut feederVoltageCtrlReq = new VoltageOut(0).withEnableFOC(true);
@@ -45,12 +53,15 @@ public class SpindexerIOTalonFX implements SpindexerIO {
         config.CurrentLimits.StatorCurrentLimitEnable = true;
         config.CurrentLimits.StatorCurrentLimit = 80.0;
 
-        spinMotor.getConfigurator().apply(config);
+        spinMotor1.getConfigurator().apply(config);
 
         config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         config.Feedback.SensorToMechanismRatio = FEEDER_GEAR_RATIO;
         config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        feederMotor.getConfigurator().apply(config);
+
+        feederMotor1.getConfigurator().apply(config);
+        config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
+        feederMotor2.getConfigurator().apply(config);
 
         BaseStatusSignal.setUpdateFrequencyForAll(
                 50.0,
@@ -59,14 +70,14 @@ public class SpindexerIOTalonFX implements SpindexerIO {
                 spinSupplyCurrent,
                 spinStatorCurrent,
                 spinTemp,
-                feederVelocity,
-                feederVoltage,
-                feederSupplyCurrent,
-                feederStatorCurrent,
-                feederTemp);
+                feeder1Velocity,
+                feeder1Voltage,
+                feeder1SupplyCurrent,
+                feeder1StatorCurrent,
+                feeder1Temp);
 
-        spinMotor.optimizeBusUtilization();
-        feederMotor.optimizeBusUtilization();
+        spinMotor1.optimizeBusUtilization();
+        feederMotor1.optimizeBusUtilization();
     }
 
     @Override
@@ -81,18 +92,25 @@ public class SpindexerIOTalonFX implements SpindexerIO {
         inputs.spinTempCelsius = spinTemp.getValueAsDouble();
 
         inputs.feederMotorConnected = BaseStatusSignal.refreshAll(
-                        feederVelocity, feederVoltage, feederSupplyCurrent, feederStatorCurrent, feederTemp)
+                        feeder1Velocity, feeder1Voltage, feeder1SupplyCurrent, feeder1StatorCurrent, feeder2Temp,feeder2Velocity, feeder2Voltage, feeder2SupplyCurrent, feeder2StatorCurrent, feeder2Temp)
                 .isOK();
-        inputs.feederVelocityRPS = feederVelocity.getValueAsDouble();
-        inputs.feederAppliedVolts = feederVoltage.getValueAsDouble();
-        inputs.feederSupplyCurrentAmps = feederSupplyCurrent.getValueAsDouble();
-        inputs.feederStatorCurrentAmps = feederStatorCurrent.getValueAsDouble();
-        inputs.feederTempCelsius = feederTemp.getValueAsDouble();
+        inputs.feederVelocityRPS = feeder1Velocity.getValueAsDouble();
+        inputs.feederAppliedVolts = feeder1Voltage.getValueAsDouble();
+        inputs.feederSupplyCurrentAmps = feeder1SupplyCurrent.getValueAsDouble();
+        inputs.feederStatorCurrentAmps = feeder1StatorCurrent.getValueAsDouble();
+        inputs.feederTempCelsius = feeder1Temp.getValueAsDouble();
+
+        inputs.feederVelocityRPS = feeder2Velocity.getValueAsDouble();
+        inputs.feederAppliedVolts = feeder2Voltage.getValueAsDouble();
+        inputs.feederSupplyCurrentAmps = feeder2SupplyCurrent.getValueAsDouble();
+        inputs.feederStatorCurrentAmps = feeder2StatorCurrent.getValueAsDouble();
+        inputs.feederTempCelsius = feeder2Temp.getValueAsDouble();
     }
 
     @Override
     public void setMotorVoltages(double spinVolts, double feederVolts) {
-        spinMotor.setControl(spinVoltageCtrlReq.withOutput(spinVolts));
-        feederMotor.setControl(feederVoltageCtrlReq.withOutput(feederVolts));
+        spinMotor1.setControl(spinVoltageCtrlReq.withOutput(spinVolts));
+        feederMotor1.setControl(feederVoltageCtrlReq.withOutput(feederVolts));
+        feederMotor2.setControl(feederVoltageCtrlReq.withOutput(feederVolts));
     }
 }
